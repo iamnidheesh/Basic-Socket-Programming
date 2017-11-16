9da3db42831dccd265e1de951f728867c07a155d
Nidheesh <nidheeshpandey@gmail.com>
2017-11-16 19:31:58 +0530

    client address fetch in tcp server

--- TCPserver.c
+++ TCPserver.c
@@ -15,7 +15,7 @@ int main() {
 	server_socket = socket(AF_INET,SOCK_STREAM,0);
 
 	// define the server address.
-	struct sockaddr_in server_address;
+	struct sockaddr_in server_address,client_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY; // any ip on local machine
@@ -25,7 +25,9 @@ int main() {
 	// 3. listen
 	listen(server_socket , 5);
 	// 4. accept
-	int client_socket = accept(server_socket,NULL,NULL);
+	unsigned int len; // stores message length sent to client.
+	// client_address stores the client addr.
+	int client_socket = accept(server_socket,(struct sockaddr *)&client_address,&len);
 	// send the message
 	send(client_socket,server_message,sizeof(server_message),0);
 	
